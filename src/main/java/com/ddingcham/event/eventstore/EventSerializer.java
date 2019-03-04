35d832221839dc263a3133dfbcbcebdb6756881f
ddingcham <qo0op308@gmail.com>
2019-03-04 16:40:24 +0900

    EventSerializerSpec should parse ItemOrdered event

--- src/main/java/com/ddingcham/event/eventstore/EventSerializer.java
+++ src/main/java/com/ddingcham/event/eventstore/EventSerializer.java
@@ -1,7 +1,37 @@
 package com.ddingcham.event.eventstore;
 
+import com.ddingcham.event.domain.events.DomainEvent;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Component
 public class EventSerializer {
+
+    private final ObjectMapper objectMapper;
+
+    EventSerializer() {
+        this.objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    }
+
+    DomainEvent deserialize(EventDescriptor eventDescriptor) {
+        try {
+            return objectMapper.readValue(eventDescriptor.getBody(), DomainEvent.class);
+        } catch (IOException e) {
+            throw new UnSupportedSerializableEventException(e);
+        }
+    }
+
+    static class UnSupportedSerializableEventException extends RuntimeException {
+        public UnSupportedSerializableEventException(Throwable cause) {
+            super(cause);
+        }
+    }
 }
--- src/test/groovy/com/ddingcham/event/eventstore/EventSerializerSpec.groovy
+++ src/test/groovy/com/ddingcham/event/eventstore/EventSerializerSpec.groovy
@@ -1,13 +1,31 @@
 package com.ddingcham.event.eventstore
 
+import com.ddingcham.event.domain.events.ItemOrdered
 import spock.lang.Specification
 import spock.lang.Subject
 
+import static java.time.Instant.now
+
 class EventSerializerSpec extends Specification {
+
+    final String ANY_TYPE = "ANY_TYPE"
+    final UUID ANY_UUID = UUID.fromString("9a94d251-5fdb-4f38-b308-9f72d2355467")
 
     @Subject EventSerializer eventSerializer = new EventSerializer()
 
     def "should parse ItemOrdered event"() {
+        given:
+            String body = """{
+                "type": "$ItemOrdered.TYPE",
+                "uuid": "${ANY_UUID.toString()}",
+                "when": "2016-05-24T12:06:41.045Z",
+                "price": "123.45"
+            }"""
+        when:
+            ItemOrdered event = eventSerializer.deserialize(new EventDescriptor(body, now(), ANY_TYPE, ANY_UUID))
+        then:
+            event.uuid == ANY_UUID
+            event.price == 123.45
 
     }
 
